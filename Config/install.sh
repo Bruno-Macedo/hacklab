3b333273476b29bcecec48627c854007b0492eb7
Bruno-Macedo <bruno.macedoxxi@hotmail.com>
2024-05-15 13:35:05 +0200

    new config

--- Config/install.sh
+++ Config/install.sh
@@ -1,13 +1,15 @@
-#!bin/bash
+#!/bin/bash
 
 sudo apt-get update
 sudo apt full-upgrade -y
-echo 'kaliwork        ALL=(ALL) NOPASSWD: ALL' | sudo tee -a /etc/sudoers
-echo 'basicuser       ALL=(ALL) ALL' | sudo tee -a /etc/sudoers
+echo "${USER}        ALL=(ALL) NOPASSWD: ALL" | sudo tee -a /etc/sudoers
+#echo 'basicuser       ALL=(ALL) ALL' | sudo tee -a /etc/sudoers
 
 
 sudo apt install -y docker.io
 sudo systemctl enable docker --now
+sudo systemctl enable docker --now
+sudo usermod -aG docker $USER
 echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian bookworm stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list 
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
@@ -17,6 +19,17 @@ sudo apt install -y docker-ce docker-ce-cli containerd.io
 
 sudo apt autoremove -y
 
-wget 'https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb'
+wget 'https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb' -O ~/Downloads/Chrome.deb
 sudo dpkg -i *.deb
 sudo apt-get install -f
+
+sudo apt-get install xclip -y
+
+sudo cp ~/workspace/hacklab/Config/vpn /usr/bin/vpn
+
+cp ~/workspace/hacklab/Config/.tmux.conf ~/.tmux.conf
+tmux source-file ~/.tmux.conf
+
+sudo apt-get  install ufw
+sudo ufw enable
+sudo apt-get install gufw
