4855559974f1a92c665b40b1262244ff9974fd65
Bruno-Macedo <bruno.macedoxxi@hotmail.com>
2024-05-15 08:15:00 +0200

    basic install

---
+++ Config/install.sh
@@ -0,0 +1,22 @@
+#!bin/bash
+
+sudo apt-get update
+sudo apt full-upgrade -y
+echo 'kaliwork        ALL=(ALL) NOPASSWD: ALL' | sudo tee -a /etc/sudoers
+echo 'basicuser       ALL=(ALL) ALL' | sudo tee -a /etc/sudoers
+
+
+sudo apt install -y docker.io
+sudo systemctl enable docker --now
+echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian bookworm stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list 
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io
+
+sudo apt autoremove -y
+
+#wget 'https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64'
+#sudo dpkg -i *.deb
+#sudo apt-get install -f
